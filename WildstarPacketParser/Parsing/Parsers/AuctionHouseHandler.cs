f2ac03e84565bca9e98c1040f1e64920d2cf971c
MaxtorCoder <warsongkiller.s8@gmail.com>
2020-09-24 19:42:42 +0200

    Add 1 Auctionhouse Packet (need to finish)

---
+++ WildstarPacketParser/Parsing/Parsers/AuctionHouseHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WildstarPacketParser.Constants;
+using WildstarPacketParser.Network;
+using WildstarPacketParser.Network.Message;
+
+namespace WildstarPacketParser.Parsing.Parsers
+{
+    public static class AuctionHouseHandler
+    {
+        [Message(Opcodes.ServerOwnedItemAuctions)]
+        public static void HandleOwnedItemsResponse(Packet packet)
+        {
+            var itemCount = packet.ReadUInt("ItemCount");
+        }
+    }
+}
